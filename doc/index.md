98820f18ecdd0f23e0a656ecfbed1b01db83d2f2
Aymane DANIEL <aymane.daniel@ebp.com>
2023-08-21 18:51:44 +0200

    Add jest config and first test unit

--- doc/index.md
+++ doc/index.md
@@ -1,4 +1,9 @@
-command
-`npm run build ` execute le script build dans package.json 
+## command build
+* `npm run build ` execute le script build dans package.json
+* `npm link ` creer un alias avec le champs bin de package.jsn et le npm global 
 
-`npm link ` creer un alias avec le champs bin de package.jsn et le npm global 
+## command jest
+* `npm install jest ts-jest `  install la libraire de test Jest
+* `npm install -D @types/jest`
+* `npx ts-jest config:init ` pour configurer les test 
+* `npx jst` lance les tests
---
+++ src/tests/posting-message.spec.ts
@@ -0,0 +1,50 @@
+describe("Feature: Posting a message", () => {
+    describe("Rule: A message can contain of 280 characters", () => {
+        test("Alice can post a message on her timeline", () => {
+
+            givenNowIs(new Date("2023-01-19T19:00:00.000Z"));
+
+            whenUserPostsAmessage({
+                id: "message-id",
+                text: "Hello World",
+                author: "Alice",
+            });
+
+            thenPostedMessageShouldBe({
+                id: "message-id",
+                author: "Alice",
+                text: "Hello World",
+                publishedAt: new Date("2023-01-19T19:00:00.000Z"),
+            });
+
+        })
+    })
+});
+let message: { id: string; author: string; text: string; publishedAt: Date };
+let now: Date;
+
+function givenNowIs(_now: Date) {
+    now = _now;
+}
+
+function whenUserPostsAmessage(postMessageCommand: {
+    id: string;
+    text: string;
+    author: string;
+}) {
+    message = {
+        id: postMessageCommand.id,
+        text: postMessageCommand.text,
+        author: postMessageCommand.author,
+        publishedAt: now,
+    };
+}
+
+function thenPostedMessageShouldBe(expectedMessage: {
+    id: string;
+    author: string;
+    text: string;
+    publishedAt: Date;
+}) {
+    expect(expectedMessage).toEqual(message);
+}
